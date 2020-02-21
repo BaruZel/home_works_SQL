9a8855d775992f2483824f8819f7006f165b4048
Bara Zelenkova <barazelenkova11@gmail.com>
2020-02-21 19:29:27 +0100

    Creating of a user name

--- Script_1.sql
+++ Script_1.sql
@@ -2,11 +2,15 @@ CREATE TABLE sakila.public.customer_new
 AS 
 SELECT *
 FROM customer c 
-
-
 ;
 
+SELECT user_name, first_name, last_name 
+FROM customer
+;
 
-
-
-
+ALTER TABLE customer 
+	ADD COLUMN user_name varchar (8)
+;
+UPDATE customer 
+SET user_name = concat(LEFT (first_name, 1), LEFT (last_name, 7))
+;
