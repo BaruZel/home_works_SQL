b0db1f0e2caa4d937adc2360c479696e62721f3e
Bara Zelenkova <barazelenkova11@gmail.com>
2020-02-21 19:59:12 +0100

    creating a column with a full name(first letter -UPPER, rest -LOWER)

--- Script_1.sql
+++ Script_1.sql
@@ -4,7 +4,7 @@ SELECT *
 FROM customer c 
 ;
 
-SELECT user_name, first_name, last_name 
+SELECT user_name, first_name, last_name,full_name 
 FROM customer
 ;
 
@@ -14,3 +14,13 @@ ALTER TABLE customer
 UPDATE customer 
 SET user_name = concat(LEFT (first_name, 1), LEFT (last_name, 7))
 ;
+
+ALTER TABLE customer 
+	ADD COLUMN full_name varchar (70)
+;
+
+UPDATE customer 
+SET full_name = concat(upper(substring(first_name,1,1)), lower(substring(first_name,2,length(first_name))), ' ', upper(substring(last_name,1,1)), lower(substring(last_name,2,length(last_name))))
+
+;
+
