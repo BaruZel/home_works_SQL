21936f77235a9fabbd7afb022cb35faa4587ce3b
Bara Zelenkova <barazelenkova11@gmail.com>
2020-03-06 20:25:23 +0100

    Update the table with new entry

--- Script_2.sql
+++ Script_2.sql
@@ -3,3 +3,18 @@ AS
 SELECT * 
 FROM customer c 
 ;
+INSERT INTO customer_backup (
+	customer_id,
+	first_name, 
+	last_name, 
+	email)
+VALUES 
+(	600, 
+	'Barbora', 
+	'Zelenkova', 
+	'barazelenkova@seznam.cz')
+	
+;
+SELECT *
+FROM customer_backup cb 
+;
